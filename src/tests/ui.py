ef3eadd4be979855cd7a1c7bad859af420e6cd8d
leonbein <leon.bein@tum.de>
2025-10-16 17:25:29 +0200

    Add first ui test (to be added to ci)

---
+++ src/tests/ui.py
@@ -0,0 +1,17 @@
+import time
+
+import reacton
+import reacton.ipywidgets as w
+from IPython.display import display
+
+import playwright.sync_api
+from playwright.sync_api import expect
+
+from karibdis.Application import *
+
+def test_app_runs(solara_test, page_session: playwright.sync_api.Page):
+    app = JupyterApplication()
+    display(app.base_view())
+    expect(page_session.get_by_text('Knowledge Modeling')).to_be_visible()
+    expect(page_session.get_by_text('Process Execution')).to_be_visible()
+    expect(page_session.get_by_text('Explore Graph')).to_be_visible()
