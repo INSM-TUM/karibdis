61b1fa280f773345571b153a4deb79cfa3f9d04a
manu6287 <shreyanshjha65@gmail.com>
2025-11-10 15:08:40 +0100

    Delete old test file

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,7 +1,4 @@
-
-# filepath: c:\Users\wwwdu\Desktop\___BPM_Hiwi\karibdis\src\tests\conftest.py
 import sys
 from pathlib import Path
 
-# ensure the project 'src' directory is on sys.path for test collection
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
