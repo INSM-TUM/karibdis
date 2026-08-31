605ed14d56cc4e990f7d39e84593346e5b5abf43
leonbein <leon.bein@tum.de>
2026-08-31 19:08:15 +0200

    CI Increase application test timeout

--- src/tests/ui/Application_test.py
+++ src/tests/ui/Application_test.py
@@ -13,7 +13,7 @@ def test_app_runs(solara_test, page_session: Page):
     app = JupyterApplication()
     display(app.base_view())
     tablist = page_session.get_by_role('tablist')
-    expect(tablist.get_by_text('Knowledge Modeling')).to_be_visible()
+    expect(tablist.get_by_text('Knowledge Modeling')).to_be_visible(timeout=10000)
     expect(tablist.get_by_text('Decisionmaking')).to_be_visible()
     expect(tablist.get_by_text('Task Execution')).to_be_visible()
     expect(tablist.get_by_text('Explore Graph')).to_be_visible() # TODO when running headed, is completely misplaced
